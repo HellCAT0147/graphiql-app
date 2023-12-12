ccfd30b32116c889bff43a7f30ae6cbd998f99eb
a-reznikov <a.reznnikov@gmail.com>
2023-12-12 18:20:30 +0300

    feat: add RTK Query to project

--- src/components/main/main.tsx
+++ src/components/main/main.tsx
@@ -1,7 +1,8 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { Context } from '../../contexts';
 import { LangContext } from '../../contexts/types';
 import { EmptyProps } from '../types';
+import { useGetDataQuery } from '../../store/reducers/api-slice';
 
 const Main: React.FC<EmptyProps> = (): JSX.Element => {
   const context: LangContext = useContext<LangContext>(Context);
@@ -9,7 +10,27 @@ const Main: React.FC<EmptyProps> = (): JSX.Element => {
     lang: { mainTitle },
   } = context;
 
-  return <section className="main container-fluid">{mainTitle}</section>;
+  const [page, setPage] = useState<string>('1');
+
+  const { data } = useGetDataQuery({ term: '', limit: '3', page });
+
+  function onGetData() {
+    setPage(`${+page + 1}`);
+  }
+
+  return (
+    <main className="main container-fluid">
+      {mainTitle}
+      <button
+        type="button"
+        className="btn btn-outline-primary"
+        onClick={onGetData}
+      >
+        Get Data
+      </button>
+      <p>{data && JSON.stringify(data)}</p>
+    </main>
+  );
 };
 
 export default Main;
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,10 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import messageReducer from './reducers/message-slice';
+import { apiSlice } from './reducers/api-slice';
 
 export const store = configureStore({
   reducer: {
     message: messageReducer,
+    [apiSlice.reducerPath]: apiSlice.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
