bfa0a50be26bed90350a0b4eb09318a8471b33c5
a-reznikov <a.reznnikov@gmail.com>
2023-12-14 19:38:52 +0300

    feat: implement graphql request with RTK Query

--- src/components/main/main.tsx
+++ src/components/main/main.tsx
@@ -2,21 +2,33 @@ import { useContext, useState } from 'react';
 import { Context } from '../../contexts';
 import { LangContext } from '../../contexts/types';
 import { EmptyProps } from '../types';
-// import { useGetDataQuery } from '../../store/reducers/api-slice'; //TODO uncommented this line if you want to get data
+import { useGetDataQuery } from '../../store/reducers/api-slice';
 
 const Main: React.FC<EmptyProps> = (): JSX.Element => {
   const context: LangContext = useContext<LangContext>(Context);
+  const [query, setQuery] = useState('');
   const {
     lang: { mainTitle },
   } = context;
+  const querySchema = `{ __schema { types { name } } }`;
+  const queryRequest = `query { characters { info { count pages next prev } } }`;
+  const headers = {
+    'content-type': 'application/json',
+    Authorization: '<token>',
+  };
 
-  const [page, setPage] = useState<string>('1');
+  const graphqlQuery = {
+    query: query,
+    variables: {},
+  };
+  const options = {
+    url: 'https://rickandmortyapi.com/graphql',
+    method: 'POST',
+    headers: headers,
+    body: JSON.stringify(graphqlQuery),
+  };
 
-  // const { data } = useGetDataQuery({ term: '', limit: '3', page }); //TODO uncommented this line if you want to get data
-
-  function onGetData() {
-    setPage(`${+page + 1}`);
-  }
+  const { data } = useGetDataQuery({ ...options });
 
   return (
     <main className="main container-fluid">
@@ -24,12 +36,18 @@ const Main: React.FC<EmptyProps> = (): JSX.Element => {
       <button
         type="button"
         className="btn btn-outline-primary"
-        onClick={onGetData}
+        onClick={() => setQuery(queryRequest)}
       >
         Get Data
       </button>
-      {/* {//TODO uncommented next line if you want to show data} */}
-      {/* <p>{data && JSON.stringify(data)}</p> */}
+      <button
+        type="button"
+        className="btn btn-outline-primary"
+        onClick={() => setQuery(querySchema)}
+      >
+        Get Schema
+      </button>
+      <p>{data && JSON.stringify(data)}</p>
     </main>
   );
 };
