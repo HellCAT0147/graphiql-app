75723e03d9c517943347f66c8eeab1b4610dc89e
a-reznikov <a.reznnikov@gmail.com>
2024-01-06 11:41:08 +0300

    feat: add animation to RS logo

--- src/components/welcome/team-intro/TeamIntro.tsx
+++ src/components/welcome/team-intro/TeamIntro.tsx
@@ -1,12 +1,15 @@
-import { ReactNode, useContext } from 'react';
+import { ReactNode, useContext, useRef } from 'react';
 import { Context } from '../../../contexts';
 import { LangContext } from '../../../contexts/types';
+import { useHoverDirty } from 'react-use';
 
 function TeamIntro(): ReactNode {
   const context: LangContext = useContext<LangContext>(Context);
   const {
     lang: { welcomeTeamTitle, welcomeTeamIntro },
   } = context;
+  const hovered = useRef(null);
+  const isHovering = useHoverDirty(hovered);
 
   return (
     <section className="team-intro d-flex flex-row justify-content-center">
@@ -14,11 +17,23 @@ function TeamIntro(): ReactNode {
         <h2 className="text-secondary">{welcomeTeamTitle}</h2>
         <p>{welcomeTeamIntro}</p>
       </div>
-      <div className="d-flex flex-column align-items-center justify-content-center text-center ">
-        <a href="https://rs.school/" target="_blank" rel="noreferrer">
-          <img src="/logo-rs.svg" alt="logo"></img>
+      <div className="d-flex flex-column align-items-center justify-content-center text-center">
+        <a
+          ref={hovered}
+          href="https://rs.school/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img
+            src="/logo-rs.svg"
+            alt="logo"
+            style={{
+              scale: isHovering ? '1.1' : '1.0',
+              transition: 'all 0.3s',
+            }}
+          ></img>
         </a>
-        <div className="text-secondary fs-2 "> & RS School</div>
+        <div className="text-secondary fs-2"> & RS School</div>
       </div>
     </section>
   );
