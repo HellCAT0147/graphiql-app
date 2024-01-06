6649bb6a3a3661808f986354262f64d9857dfee5
a-reznikov <a.reznnikov@gmail.com>
2024-01-06 13:24:01 +0300

    feat: add link to project repo and styles to footer

--- src/components/elements/icon-link/IconLink.tsx
+++ src/components/elements/icon-link/IconLink.tsx
@@ -5,28 +5,32 @@ function IconLink({
   href,
   faCode,
   text,
+  direction,
+  btnStyle = 'info',
 }: {
   href: string;
   faCode: string;
   text?: string;
+  direction?: string;
+  btnStyle?: string;
 }): ReactNode {
   const hovered = useRef(null);
   const isHovering = useHoverDirty(hovered);
   return (
     <a
-      className="d-flex"
+      className={`d-flex gap-2 flex-${direction}`}
       href={href}
       target="_blank"
       rel="noreferrer"
       style={{ textDecoration: 'none' }}
     >
       <i
-        className={`btn btn-info p-1 d-flex fs-6 ${faCode} align-self-center`}
+        className={`btn btn-${btnStyle} p-1 d-flex fs-6 ${faCode} align-self-center`}
       ></i>
       {text && (
         <p
           ref={hovered}
-          className="m-0 ps-2 align-self-end transition"
+          className="m-0 align-self-end transition"
           style={{
             color: isHovering ? '#3498db' : '#FFFFFF',
             lineHeight: '100%',
--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,13 +1,22 @@
 import { ReactNode } from 'react';
 import { FooterLinks } from './footer-links';
 
-import { ImgLink } from '../elements';
+import { IconLink, ImgLink } from '../elements';
 
 function Footer(): ReactNode {
   return (
     <footer className="d-flex justify-content-around align-items-center my-5 justify-self-end">
       <FooterLinks />
-      <p> 2023 </p>
+      <div>
+        <p style={{ color: '#FFFFFF' }}>HAQ © 2023</p>
+        <IconLink
+          href={'https://github.com/HellCAT0147/graphiql-app'}
+          faCode="fa-brands fa-github"
+          text={'Graphiql App'}
+          direction="column"
+          btnStyle="danger"
+        />
+      </div>
       <ImgLink
         href="https://rs.school/"
         classLink="ps-5 pe-2"
